ad3d24475f73ccb533515a7fbf09e3f99f7dc1e5
prakx <prakx2050@gnail.com>
2020-07-12 00:08:44 +0530

    Search Version 1.0

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var profileRouter = require('./routes/profileRouter');
 var updateprofileRouter = require('./routes/updateprofileRouter');
 var deletebookRouter = require('./routes/deletebookRouter');
 var updatebookRouter = require('./routes/updatebookRouter');
-var searchRouter=require('./routes/searchRouter2');
+var searchRouter=require('./routes/searchRouter');
 
 //session requirements
 var session = require('express-session');
